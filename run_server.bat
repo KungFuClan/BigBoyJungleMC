1a3ff046e3c2c8b67de019ab3d60c89ad0a7a744
Jacob <jakeywaldrip@gmail.com>
2021-02-28 15:07:44 -0600

    updated server bat to point to master

--- run_server.bat
+++ run_server.bat
@@ -1,8 +1,29 @@
 @echo off
-FOR /F %%I IN ('curl ifconfig.me') DO SET externalIP=%%I
+git fetch --all
+git reset --hard remotes/origin/master
+for /F "tokens=2" %%i in ('date /t') do set opendate=%%i
+set opentime=%time%
+if exist save.lock (
+	echo. "Save is currently in use"
+	pause
+	exit
+) else (
+	echo. "Locking save..."
+	echo. Locked on %opendate% - %opentime% > save.lock
+	git add .
+	git commit -am "Locking save - %opendate% - %opentime%"
+	git push
+)
+for /F %%I in ('curl ifconfig.me') do set externalIP=%%I
 set discordURL=https://discord.com/api/webhooks/815122326520987679/BXMNpn8woJM5faksL_kcnpKNlwAu2xToLNNcXQjrO8UjTA86BIqoIAVR1mULCHmSiAfa
-set onlineMessage="Big Boy Jungle is LIVE at %externalIP% - Come chill!"
-set offlineMessage="Big Boy Jungle is SIGNING OFF %externalIP% is offline."
+set onlineMessage="MC Server is live at %externalIP% - Come join!"
+set offlineMessage="MC Server at %externalIP% is offline."
 curl -X POST -d content=%onlineMessage% %discordURL%
 call java -Xmx1024M -Xms1024M -jar server.jar /wait
 curl -X POST -d content=%offlineMessage% %discordURL%
+del save.lock
+for /F "tokens=2" %%i in ('date /t') do set exitdate=%%i
+set exittime=%time%
+git add .
+git commit -am "Server Upload - %exitdate% - %exittime%"
+git push
